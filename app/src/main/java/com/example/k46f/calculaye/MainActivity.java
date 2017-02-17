4168918173e6e5f821bcdc8b60f932c11a2bbae5
John Ramirez <k46f@hotmail.com>
2017-02-17 22:32:32 +0800

    Added Sound

--- app/src/main/java/com/example/k46f/calculaye/MainActivity.java
+++ app/src/main/java/com/example/k46f/calculaye/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private Double numberOne = 0.0;
     private Double numberTwo = 0.0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
